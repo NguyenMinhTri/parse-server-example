1cdba07069a258ac64d9ee0a75bca85bf6efc131
NguyenMinhTri <nmtri.uit@gmail.com>
2017-01-04 00:10:12 +0700

    ....

--- index.js
+++ index.js
@@ -20,7 +20,18 @@ var api = new ParseServer({
   liveQuery: {
     classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
   }
-  
+   push: {
+      android: {
+        senderId: 'dictplus-63839',
+        apiKey: 'AIzaSyCfxMYIArAi3fQM4qYGjezAzPu-AQyPbn8'
+      },
+      ios: {
+        pfx: '/file/path/to/XXX.p12',
+        passphrase: '', // optional password to your p12/PFX
+        bundleId: '',
+        production: false
+      }
+    }
 });
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
 // If you wish you require them, you can set them as options in the initialization above:
